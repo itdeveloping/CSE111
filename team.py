f464f67284bbca140153c2e28e6b663c60f917d4
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-01-10 22:56:59 -0600

    work in team,

---
+++ team.py
@@ -0,0 +1,5 @@
+""""
+
+If the subtotal is $50 or greater and today is Tuesday or , your program must subtract 10% from the subtotal. Your program must then compute the total amount due by adding sales tax of 6% to the subtotal. Your program must print the discount amount if applicable, the sales tax amount, and the total amount due.
+
+"""
