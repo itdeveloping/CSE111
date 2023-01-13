d3954febc749a6a1cbca0e2daf0c2e40087baaf1
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-01-12 18:00:37 -0600

    change

--- tire_volume.py
+++ tire_volume.py
@@ -1,6 +1,6 @@
 
 """
-File: heart_rate.py
+File: tire_volume.py
 Author: Oscar Rodriguez
 
 Purpose: Prove that you can write a Python program that gets input from a user, 
@@ -21,17 +21,43 @@ w is the width of the tire in millimeters,
 a is the aspect ratio of the tire, and
 d is the diameter of the wheel in inches.
 """
+# Import the datetime class from the datetime
+from datetime import datetime
 
+#import math library
 import math
-# Get the size of a car tire
 
+# Get the size of a car tire
 w=float(input("Enter the width of the tire in mm (ex 205): "))
 a=float(input("Enter the aspect ratio of the tire (ex 60):"))
 d=float(input("Enter the diameter of the wheel in inches (ex 15):"))
 
-# Calculate 
+pricetext=""
+answer=""
+phone=""
+prices=[[235,65,17,150.99],[245,55,18,113.93],[235, 65, 16, 109.35],[195, 65, 15, 60.99]]
+for price in prices:
+    if price[0]==w and price[1]==a and price[2]==d:
+        print(f"The price of this tire is: ${price[3]}")
+        pricetext=", $"+str(price[3])
+
+        # Calculate 
+        while answer!="yes" and answer!="no":
+            answer=input("Do you want to buy this tire (yes/no)?").lower()
+            if answer=="yes":
+                phone=input("What is your phone number? ")
+                phone=", "+phone
+
 v=(math.pi*w**2*a*(w*a+2540*d))/10000000000
 
-print (f"The approximate volume is {v:.2f} liters")
+#print (f"The approximate volume is {v:.2f} liters")
 
+# Call the now() method to get the current date and time as a datetime object from the computer's operating system.
+current_date_and_time = datetime.now()
 
+with open("volumes.txt", "at") as textfile:
+    # Code to do something with each line goes here
+    print(f"{current_date_and_time:%Y-%m-%d}, {w:.0f}, {a:.0f}, {d:.0f}, {v:.2f}{pricetext}{phone}", file=textfile)
+    print("This data has been append to the file volumes.txt")
+
+print("Thank you for using our systems!")
