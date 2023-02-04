820220b82d983674f423a1eca2626663ae2bc38f
itdeveloping <oscar.rodriguez.alfaro@hotmail.com>
2023-02-04 01:33:46 -0600

    sat

---
+++ sentences.py
@@ -0,0 +1,118 @@
+"""
+Author: Oscar Jose Rodriguez Alfaro
+CSE 111 | Programming with Functions
+File name: sentences.py
+
+Purpose
+Prove that you can write a Python program and write and run test functions to help you find and fix mistakes.
+"""
+
+import random
+
+# Define get_determiner function
+def get_determiner(quantity):
+    """Return a randomly chosen determiner. A determiner is
+    a word like "the", "a", "one", "some", "many".
+    If quantity is 1, this function will return either "a",
+    "one", or "the". Otherwise this function will return
+    either "some", "many", or "the".
+
+    Parameter
+        quantity: an integer.
+            If quantity is 1, this function will return a
+            determiner for a single noun. Otherwise this
+            function will return a determiner for a plural
+            noun.
+    Return: a randomly chosen determiner.
+    """
+    # Make a list of determiner based on the arguments
+    if quantity == 1:
+        words = ["a", "one", "the"]
+    else:
+        words = ["some", "many", "the"]
+
+    # Randomly choose and return a determiner.
+    word = random.choice(words)
+    return word
+
+# Define get_noun function
+def get_noun(quantity):
+    """Return a randomly chosen noun.
+    If quantity is 1, this function will
+    return one of these ten single nouns:
+        "bird", "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"
+    Otherwise, this function will return one of
+    these ten plural nouns:
+        "birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"
+
+    Parameter
+        quantity: an integer that determines if
+            the returned noun is single or plural.
+    Return: a randomly chosen noun.
+    """
+    # Make a list of nouns based on the arguments
+    if quantity == 1:
+        nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+    else:
+        nouns = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+
+    # Randomly choose and return a noun.
+    noun = random.choice(nouns)
+    return noun
+
+# Define get_verb function
+def get_verb(quantity, tense):
+    """Return a randomly chosen verb. If tense is "past",
+    this function will return one of these ten verbs:
+        "drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"
+    If tense is "present" and quantity is 1, this
+    function will return one of these ten verbs:
+        "drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"
+    If tense is "present" and quantity is NOT 1,
+    this function will return one of these ten verbs:
+        "drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"
+    If tense is "future", this function will return one of
+    these ten verbs:
+        "will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"
+
+    Parameters
+        quantity: an integer that determines if the
+            returned verb is single or plural.
+        tense: a string that determines the verb conjugation,
+            either "past", "present" or "future".
+    Return: a randomly chosen verb.
+    """
+
+    # Make a verb list based on the arguments 
+    if tense=="past":
+        verbs = ["drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"]
+    elif tense=="present" and quantity==1:
+        verbs = ["drinks", "eats", "grows", "laughs", "thinks", "runs", "sleeps", "talks", "walks", "writes"]
+    elif tense=="present" and quantity!=1:
+        verbs= ["drink", "eat", "grow", "laugh", "think", "run", "sleep", "talk", "walk", "write"]
+    elif tense=="future":
+        verbs=["will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk", "will walk", "will write"]
+
+    # Randomly choose and return a verb.
+    verb = random.choice(verbs)
+    return verb
+
+#define main function
+def main():
+
+    # Make a list to use it as paramaters
+    list = ([1,'past'],[1,'present'],[1,'future'],[2,'past'],[2,'present'],[2,'future'])
+    for elements in list:
+       quantity=elements[0]
+       tense=elements[1]
+       print (get_determiner(quantity).capitalize(), get_noun(quantity), get_verb(quantity, tense))
+
+#calls the main function
+main()
---
+++ test_sentences.py
@@ -0,0 +1,166 @@
+"""
+Author: Oscar Jose Rodriguez Alfaro
+CSE 111 | Programming with Functions
+File name: test_sentences.py
+
+Purpose
+Prove that you can write a Python program and write and run test functions to help you find and fix mistakes.
+"""
+
+from sentences import get_determiner, get_noun, get_verb
+import random
+import pytest
+
+
+def test_get_determiner():
+    # 1. Test the single determiners.
+
+    single_determiners = ["a", "one", "the"]
+
+    # This loop will repeat the statements inside it 4 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for _ in range(4):
+
+        # Call the get_determiner function which
+        # should return a single determiner.
+        word = get_determiner(1)
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the single_determiners list.
+        assert word in single_determiners
+
+    # 2. Test the plural determiners.
+
+    plural_determiners = ["some", "many", "the"]
+
+    # This loop will repeat the statements inside it 4 times.
+    for _ in range(4):
+
+        # Call the get_determiner function which
+        # should return a plural determiner.
+        word = get_determiner(2)
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the plural_determiners list.
+        assert word in plural_determiners
+
+def test_get_noun():
+    # 1. Test the single noun.
+
+    single_nouns = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+
+    # This loop will repeat the statements inside it 11 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for _ in range(11):
+
+        # Call the get_noun function which
+        # should return a single determiner.
+        word = get_noun(1)
+
+        # Verify that the word returned from get_noun
+        # is one of the words in the single_determiners list.
+        assert word in single_nouns
+
+    # 2. Test the plural nouns.
+
+    plural_nouns = [ "birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+
+    # This loop will repeat the statements inside it 11 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for _ in range(11):
+
+        # Call the get_noun function which
+        # should return a plural noun.
+        word = get_noun(2)
+
+        # Verify that the word returned from get_noun
+        # is one of the words in the plural_nouns list.
+        assert word in plural_nouns
+
+def test_get_verb():
+
+    # 1. Test the past tense verb.
+    past_tense = ["drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"]  
+    
+    # This loop will repeat the statements inside it 11 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for _ in range(11):
+
+        # Call the get_verb function which
+        # should return a plural determiner.
+        tense = "past"
+        quantity = 1
+        word = get_verb(quantity, tense)
+
+        # Verify that the word returned from get_verb
+        # is one of the words in the past_tense list.
+        assert word in past_tense
+
+    # 2. Test the singular present tense.
+    singular_present_tense = ["drinks", "eats", "grows", "laughs", "thinks", "runs", "sleeps", "talks", "walks", "writes"]  
+
+    # This loop will repeat the statements inside it 11 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for _ in range(11):
+
+        # Call the get_verb function which
+        # should return a singular present tense verb.
+        tense = "present"
+        quantity = 1
+        word = get_verb(quantity, tense)
+
+        # Verify that the word returned from get_verb
+        # is one of the words in the singular_present_tense list.
+        assert word in singular_present_tense
+
+    # 3. Test the plural present tense.
+    plural_present_tense = ["drink", "eat", "grow", "laugh", "think", "run", "sleep", "talk", "walk", "write"]
+
+    # This loop will repeat the statements inside it 11 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for _ in range(11):
+
+        # Call the get_verb function which
+        # should return a plural present tense verb.
+        tense = "present"
+        quantity = 2
+        word = get_verb(quantity, tense)
+
+        # Verify that the word returned from get_verb
+        # is one of the words in the plural_present_tense list.
+        assert word in plural_present_tense
+
+    # 4. Test the future tense.
+    future_tense = ["will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk", "will walk", "will write"]
+    
+    # This loop will repeat the statements inside it 11 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.    
+    for _ in range(11):
+
+        # Call the get_verb function which
+        # should return a future tense verb.
+        tense = "future"
+        quantity = 1
+        word = get_verb(quantity, tense)
+
+        # Verify that the word returned from get_verb
+        # is one of the words in the future_tense list.
+        assert word in future_tense
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
